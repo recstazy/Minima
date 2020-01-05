63ddc3e1e3f455451f9e8c87584212422fa8bbd4
recstazy <sbukotkin@yandex.ru>
2020-01-05 22:32:32 +0700

    Added base for room generation

---
+++ Assets/Scripts/LevelGeneration/LevelGeneratorBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneratorBase : MonoBehaviour
+{
+    #region Fields
+    
+    #endregion
+
+    #region Properties
+    
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LevelGeneration/WallsGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsGenerator : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private GameObject wallPrefab;
+
+    #endregion
+
+    public void CreateWalls()
+    {
+        InstantiateWall(new Vector2(3f, 3f));
+    }
+
+    protected virtual GameObject InstantiateWall(Vector2 position)
+    {
+        var wall = Instantiate(wallPrefab, position, Quaternion.identity, transform);
+        return wall;
+    }
+}
