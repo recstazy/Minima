7ab94ba10de5251a794857a944079d3e2b733b70
recstazy <sbukotkin@yandex.ru>
2020-01-23 22:18:46 +0700

    Deleted all code of mesh building

--- Assets/Scripts/Navigation/NavMeshBuilder.cs
+++ Assets/Scripts/Navigation/NavMeshBuilder.cs
@@ -25,7 +25,7 @@ namespace Minima.Navigation
 
         void Start()
         {
-            //BuildNavMesh();
+            BuildNavMesh();
         }
 
         protected virtual void Update()
@@ -40,133 +40,13 @@ namespace Minima.Navigation
         {
             System.DateTime startTime = System.DateTime.Now;
 
-            CreateBounds(buildArea, createEdges: true);
-            CreateObstaclesBounds();
-            CreateTriangles();
+            
 
             System.TimeSpan timeElapsed = System.DateTime.Now - startTime;
             Debug.Log("NavMesh building took " + timeElapsed);
         }
 
-        protected void CreateTriangles()
-        {
-            bool success;
-            int iterations = 0;
-
-            do
-            {
-                success = false;
-                var edgesTemp = edges.ToList();
-
-                foreach (var e in edgesTemp)
-                {
-                    var complete = CreateTriangle(e);
-                    if (complete)
-                    {
-                        success = true;
-                    }
-                }
-                iterations++;
-            }
-            while (success && iterations < preBuildIterations);
-
-            AddMissingEdges();
-            CleanUpPoints();
-        }
-
-        private void AddMissingEdges()
-        {
-            foreach (var origin in points.ToList())
-            {
-                var connected = origin.ConnectedPoints;
-                connected.Remove(origin);
-
-                foreach (var nextPoint in connected)
-                {
-                    var nextConnected = nextPoint.ConnectedPoints;
-                    nextConnected.RemoveAll(x => x == origin || x == nextPoint);
-
-                    foreach (var thirdPoint in nextConnected)
-                    {
-                        if (thirdPoint.ConnectedPoints.Contains(origin))
-                        {
-                            continue;
-                        }
-
-                        CreateEdge(origin, thirdPoint);
-
-                        if (origin.ConnectedPoints.Count > 100)
-                        {
-                            throw new System.Exception("NavMeshBuilder: Origin.ConnectedPoints.Count > 100");
-                        }
-                    }
-                }
-            }
-        }
-
-        private void CleanUpPoints()
-        {
-            foreach (var p in points.ToList())
-            {
-                if (p.ConnectedEdges.Count == 0)
-                {
-                    var point = GetClosestPoints(p, 1);
-                    var edge = CreateEdge(p, point[0]);
-
-                    if (edge.IsValid)
-                    {
-                        var thirdPoints = GetClosestPoints(edge.End.ConnectedPoints, p, 1);
-                        var thirdEdge = CreateEdge(thirdPoints[0], p);
-
-                        if (thirdEdge.IsValid)
-                        {
-                        }
-                        else
-                        {
-                            edges.Remove(edge);
-                            points.Remove(p);
-                        }
-                    }
-                    else
-                    {
-                        points.Remove(p);
-                    }
-                }
-            }
-        }
-
-        protected bool CreateTriangle(NavEdge edge)
-        {
-            var point = GetClosestPoint(edge);
-            return CreateTriangle(point, edge.Start, edge.End);
-        }
-
-        protected void CreateTriangle(NavPoint origin)
-        {
-            var firstPoints = GetClosestPoints(origin, 2);
-
-            if (firstPoints.Count < 2)
-            {
-                return;
-            }
-
-            CreateTriangle(origin, firstPoints[0], firstPoints[1]);
-        }
-
-        protected bool CreateTriangle(NavPoint a, NavPoint b, NavPoint c)
-        {
-            var ab = CreateEdge(a, b);
-            var ac = CreateEdge(a, c);
-            var bc = CreateEdge(b, c);
-
-            if (!ab.IsValid || !ac.IsValid || !bc.IsValid)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
+        
         protected NavPoint GetClosestPoint(NavEdge edge)
         {
             var closest = GetClosestPoints(edge.Start, 3, edge.End);
