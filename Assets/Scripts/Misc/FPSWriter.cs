3f115b45e65dae926d137de1ba2f378accae2e3c
recstazy <sbukotkin@yandex.ru>
2020-03-01 14:08:54 +0700

    Added fps in corner of screen

---
+++ Assets/Scripts/Misc/FPSWriter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSWriter : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private Text fpsText;
+
+    #endregion
+
+    #region Properties
+    
+    #endregion
+
+    void Update()
+    {
+        fpsText.text = (1 / Time.deltaTime).ToString("F0");
+    }
+}
